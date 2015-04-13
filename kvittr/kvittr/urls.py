d58c926a1ac0907674bb401a87d7649a71a89972
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-04-13 14:49:27 +0200

    added urls for testing. import patterns and setup theme.urls

--- kvittr/kvittr/urls.py
+++ kvittr/kvittr/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'kvittr.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-]
+urlpatterns = patterns('',
+    url(r'^$', include('theme.urls')),
+    url(r'^useraccounts/', include('useraccounts.urls')),
+)

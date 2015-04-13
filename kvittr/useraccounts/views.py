dbf380ced730110ae5ba0b7c54cae2e843b1dda9
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-04-13 15:44:29 +0200

    set up views.py for testing user information

--- kvittr/useraccounts/views.py
+++ kvittr/useraccounts/views.py
@@ -1,3 +1,25 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+from useraccounts.models import User
 
 # Create your views here.
+def user_listing(request):
+    useraccounts = User.objects.all()
+    context = {'useraccounts': useraccounts}
+    return render(request, 'useraccounts/user_listing.html', context)
+
+def user_details(request, useraccounts_id):
+    useraccounts = User.objects.get(pk=useraccounts_id)
+    context = {'useraccounts': useraccounts}
+    return render(request, 'useraccounts/student_details.html', context)
+
+@csrf_exempt
+def user_increase_like(request, useraccounts_id):
+	accounts = User.objects.get(pk=useraccounts_id)
+	useraccounts.passed_exams = useraccounts.passed_exams + 1
+	useraccounts.save()
+	data = {'like_updated': useraccounts.passed_exams}
+	return JsonResponse(data)
